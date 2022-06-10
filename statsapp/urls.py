8e0bc28e66c849ca43a6f0f1ae0a6fcd577a77a6
qwertyusern <xojiakbargoipov4@gmail.com>
2022-06-10 17:17:45 +0500

    m

--- statsapp/urls.py
+++ statsapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import *
-# urlpatterns = [
+urlpatterns = [
 #     path('mahsulot/', MahsulotView.as_view(), name="mahsulot"),
 #     path('client/', ClientView.as_view(), name="login"),
-# ]
+]
